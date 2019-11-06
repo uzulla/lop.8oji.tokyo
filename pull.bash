39bac994fdf01fdd926523610211df1882084751
uzulla / Junichi Ishida <uzulla@himitsukichi.com>
2019-11-06 10:55:49 +0900

    Update pull.bash

--- pull.bash
+++ pull.bash
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-date +"%Y%d%m%H%M%S" >> ~/lop.8oji.tokyo.pull.log 2>&1
-git -c core.sshCommand="ssh -i ~/.ssh/hoge_deploy.key" \
-    --git-dir=${HOME}/web/lop.8oji.tokyo/.git \
-    --work-tree=${HOME}/web/lop.8oji.tokyo/ pull \
-    >> ~/lop.8oji.tokyo.pull.log 2>&1
+git \
+  -c core.sshCommand="ssh -i ~/.ssh/deploy.key -o \"VerifyHostKeyDNS=no\" -o \"StrictHostKeyChecking=no\" -F /dev/null" \ 
+  -C ~/www/example.jp/ \
+  pull
